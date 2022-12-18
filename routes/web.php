6f19fdf034afd73ea53024b41fc1ed45d2a44b9d
nan74k <nanta.pslove@gmail.com>
2022-12-18 21:03:54 +0700

    123

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [MessagesController::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
